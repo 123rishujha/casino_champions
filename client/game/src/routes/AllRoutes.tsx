28171a5371617da46b1430cd968639003d456eaa
Rishu jha <jharishu796@gmail.com>
2023-04-08 18:23:29 +0000

    redux now working for chance and chane button size increased

--- client/game/src/routes/AllRoutes.tsx
+++ client/game/src/routes/AllRoutes.tsx
@@ -1,23 +1,17 @@
-import { Route,Routes } from 'react-router-dom';
-import Home from '../pages/Home';
-import Signin from '../components/signin';
-import Signup from '../components/signupp';
-import { Gamepage } from '../pages/Gamepage';
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Signin from "../components/signin";
+import Signup from "../components/signupp";
+import { Gamepage } from "../pages/Gamepage";
 
-
-
-const AllRoutes = () =>{
-    return(
-        <Routes>
-            <Route path='/' element={<h1>home</h1>} /> {/* game page */}
-            <Route path='/login' element={<Signin/>} />  
-            <Route path='/register' element={<Signup/>} /> 
-            <Route path='/' element={<h1>Home</h1>} /> {/* game page */}
-            <Route path='/login' element={<h1>login</h1>} />  
-            <Route path='/register' element={<h1>register</h1>} /> 
-        </Routes>
-    )
-}
+const AllRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>home</h1>} /> {/* game page */}
+      <Route path="/login" element={<Signin />} />
+      <Route path="/register" element={<Signup />} />
+    </Routes>
+  );
+};
 
 export default AllRoutes;
-
