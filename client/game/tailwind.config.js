ce4699254f1ad89e1c80769c16c6a3c53b6998dd
Sachin Kesarwani <112760394+Sachin-Kesarwani@users.noreply.github.com>
2023-04-09 18:50:47 +0530

    some changes

--- client/game/tailwind.config.js
+++ client/game/tailwind.config.js
@@ -9,22 +9,22 @@
 
 // tailwind.config.js
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       backgroundImage: {
-        'gaming': 'url(\'https://i.pinimg.com/736x/78/ce/89/78ce89d511c87c48d3f4d308617d2d90.jpg\')',
+        // 'gaming': 'url(\'https://i.pinimg.com/736x/78/ce/89/78ce89d511c87c48d3f4d308617d2d90.jpg\')',
       },
       colors: {
-        'glass': 'rgba(255, 255, 255, 0.5)',
+        glass: "rgba(255, 255, 255, 0.5)",
       },
       boxShadow: {
-        'glass': '0 0 20px 5px rgba(255, 255, 255, 0.25)',
+        glass: "0 0 20px 5px rgba(255, 255, 255, 0.25)",
       },
       fontFamily: {
-        'sans': ['Inter', 'sans-serif'],
+        sans: ["Inter", "sans-serif"],
       },
     },
   },
