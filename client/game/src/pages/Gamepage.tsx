ce591a6ac1c16be0256e744213414b1f6317ce8c
Sachin Kesarwani <112760394+Sachin-Kesarwani@users.noreply.github.com>
2023-04-09 15:46:36 +0530

    Update Gamepage.tsx

--- client/game/src/pages/Gamepage.tsx
+++ client/game/src/pages/Gamepage.tsx
@@ -50,22 +50,7 @@ console.log(noOfchances,"no")
      <div className="flex rounded-lg gap-2.5 mx-auto lg:w-1/2 sm:w-full pl-2 border-indigo-600 shadow-lg shadow-cyan-500/50">
         {bulb?.map((e, i) => {
           return (
-            <svg
-              key={i}
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-            
-              className={`fill-${e === 1 ?`yellow`:`blue`}-300 md:fill-${e === 1 ? `yellow`:`blue`}-300 lg:w-20 lg:h-20 sm:w-40 sm:w-40 m-2`}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
-              />
-            </svg>
+              <HiLightBulb color={ e==0?"white":`yellow`} fontSize={"90px"}/>
           );
         })}
       </div>
