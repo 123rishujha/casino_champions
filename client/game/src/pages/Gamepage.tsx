4bae5b91b14e87fa81da31d072dead094ec96314
Sachin Kesarwani <112760394+Sachin-Kesarwani@users.noreply.github.com>
2023-04-09 18:31:26 +0530

    score board done littltle things remain

--- client/game/src/pages/Gamepage.tsx
+++ client/game/src/pages/Gamepage.tsx
@@ -7,13 +7,17 @@ import { useSelector } from "react-redux";
 import { useAppSelector } from "../redux/store";
 import { useAppDispatch } from "../redux/store";
 import { chanceUpdate } from "../redux/chances/chances.actions";
+import { Button } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import ConfettiExplosion from 'react-confetti-explosion';
  export  const Gamepage = () => {
   let [bulb, setBulbs] = useState<number[]>(new Array(9).fill(0));
   let [button, setButton] = useState<number[]>([]);
   let [refresh, setrefresh] = useState<number>(0);
   let [chance, setChance] = useState<number>(0);
   let [emoji, setEmoji] = useState<number[]>(new Array(9).fill(0));
-
+  let navigate=useNavigate()
+  let [explosion,setexplosion]=useState<boolean>(false)
 let noOfchances=useAppSelector((store)=>store.chanceReducer.chance )
   function buttonval(val: number, index: number) {
 
@@ -42,41 +46,44 @@ let dispatch=useAppDispatch()
     setChance(0);
     setrefresh(0);
     dispatch(chanceUpdate(0));
+    setexplosion(false)
   }
 console.log(noOfchances,"no")
+
+function redirecttoscore(){
+  setexplosion(true)
+  setTimeout(()=>{
+    dispatch(chanceUpdate(0));
+navigate("/score")
+
+  },1500)
+}
   return (
     <div>
       <h1 style={{textAlign:"center"}}>Win By Luck</h1>
      <div className="flex rounded-lg gap-2.5 mx-auto lg:w-1/2 sm:w-full pl-2 border-indigo-600 shadow-lg shadow-cyan-500/50">
         {bulb?.map((e, i) => {
           return (
-            // <svg
-            //   key={i}
-            //   xmlns="http://www.w3.org/2000/svg"
-            //   fill="none"
-            //   viewBox="0 0 24 24"
-            //   strokeWidth={1.5}
-            //   stroke="currentColor"
             
-            //   className={`fill-${e === 1 ?`yellow`:`blue`}-300 md:fill-${e === 1 ? `yellow`:`blue`}-300 lg:w-20 lg:h-20 sm:w-40 sm:w-40 m-2`}
-            // >
-            //   <path
-            //     strokeLinecap="round"
-            //     strokeLinejoin="round"
-            //     d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
-            //   />
-            // </svg>
             <HiLightBulb color={ e==0?"white":`yellow`} fontSize={"90px"}/>
           );
         })}
       </div>
-      <div style={{ marginTop:"15px" }}>
+      <div style={{ marginTop:"15px", display:"flex"}}>
         <button
         style={{color:"white",width:"150px",margin:"auto",display:"block",textAlign:"center"}}
           onClick={restartgame}
           className="border-2 border-indigo-600 rounded-lg bg-cyan-600 pl-4 pr-4 ">
             
         Restart
+        </button>
+        <button
+        disabled={chance==0||chance!=noOfchances}
+        style={{color:"white",width:"150px",margin:"auto",display:"block",textAlign:"center"}}
+  onClick={redirecttoscore}
+          className={`border-2 border-indigo-600 rounded-lg bg-green-600 pl-4 pr-4  ${chance === 0||noOfchances!==chance ? `cursor-not-allowed` : `cursor-pointer`}`}>
+            
+     Score{explosion&&  <ConfettiExplosion />}
         </button>
       
 </div>
@@ -103,7 +110,7 @@ console.log(noOfchances,"no")
           })
         }
       </div>
-  
+<Button></Button>
     </div>
   );
 };
--- client/game/src/pages/Score.tsx
+++ client/game/src/pages/Score.tsx
@@ -1,19 +1,111 @@
-import React from 'react'
-import useWindowSize from 'react-use/lib/useWindowSize'
-import Confetti from 'react-confetti'
+import React, { useEffect, useState } from "react";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
+import { Avatar, Badge, Button, Heading, Text } from "@chakra-ui/react";
+import { GiDiamondTrophy } from "react-icons/gi";
+import ConfettiExplosion from "react-confetti-explosion";
+import { useNavigate } from "react-router-dom";
 const Score = () => {
-    const { width, height } = useWindowSize()
+  const { width, height } = useWindowSize();
+  let score = 90;
+  let [showcelebration,setshowcelbration]=useState(true)
+let navigate=useNavigate()
+  function joinname() {
+setTimeout(()=>{
+setshowcelbration(false)
+},5000)
+  }
+useEffect(()=>{
+  joinname()
+  
+},[])
+
+function redirectTogame(){
+navigate("/game")
+}
   return (
     <div>
-      <h1>Score</h1>
-      <Confetti
-      width={width}
-      height={height}
-      numberOfPieces={500}
+      <Heading textAlign={"center"}>Your Score</Heading>
+      {
+        showcelebration&&  <Confetti width={width} height={height}  numberOfPieces={500} />
+      }
+    
+      <div
+        style={{
+          height: "300px",
+          marginTop: "20px",
+        
+          borderRadius: "10px",
+          margin: "auto",
+          textAlign: "center",
+        
+         boxShadow:"rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px"
+        }}
+        className=" lg:w-1/2 sm:w-full md:w-2/3"
+      >
+        <Heading m={"auto"} color={"red.400"}>Congratulation </Heading>
+        <Heading>
+          {/* <Avatar name={`Sachin Kesarwani`} />{" "} */}
+          <Badge bg={"green.600"} p={2} borderRadius={5}>
+            <Heading as="h2" fontSize={"2xl"}>
+              Sachin Kesarwani
+            </Heading>
+          </Badge>
+        </Heading>
+        <Text>You Won...</Text>
+        {score > 0 && score < 20 ? <div style={{ width: "13%", display: "flex", margin: "auto" }}>
+        
+            <GiDiamondTrophy  fontSize={"80px"}  color="blue"/>
+            <ConfettiExplosion/>
+          </div> : score > 20 && score < 40 ? (
+          <div style={{ width: "26%", display: "flex", margin: "auto"}}>
+            <GiDiamondTrophy fontSize={"80px"} color="blue"/>
+            <ConfettiExplosion/>
+            <GiDiamondTrophy fontSize={"80px"} color="blue"/>
+         
+          </div>
+        ) : score > 40 && score < 60 ? (
+          <div style={{ width: "39%", display: "flex", margin: "auto" }}>
+            <GiDiamondTrophy fontSize={"80px"} color="blue"/>
+            <ConfettiExplosion/>
+            <GiDiamondTrophy  fontSize={"80px"} color="blue"/>
+            <ConfettiExplosion/>
+            <GiDiamondTrophy fontSize={"80px"} color="blue" />
+          
+          </div>
+        ) : score > 60 && score < 80 ? (
+          <div
+            style={{
+              width: "50%",
+              display: "flex",
+              margin: "auto",
+            
+            }}
+          >
+            <GiDiamondTrophy fontSize={"80px"} color="blue" />
+            <GiDiamondTrophy fontSize={"80px"} color="blue" />
+            <ConfettiExplosion/>
+            <GiDiamondTrophy  fontSize={"80px"} color="blue" />
+            <GiDiamondTrophy  fontSize={"80px"} color="blue"/>
+          
+          </div>
+        ) : (
+          <div style={{ width: "50%", display: "flex", margin: "auto" }}>
+            <GiDiamondTrophy fontSize={"80px"} color="blue"  />
+            <GiDiamondTrophy fontSize={"80px"} color="blue" />
+            <ConfettiExplosion/>
+            <GiDiamondTrophy fontSize={"80px"} color="blue" />
+            <ConfettiExplosion/>
+            <GiDiamondTrophy fontSize={"80px"} color="blue" />
+            <GiDiamondTrophy fontSize={"80px"} color="blue" />
+           
+          </div>
+        )}
+         <Button onClick={redirectTogame} _hover={{bg:"green.400"}} bg={`green.400`} top={"10px"}>Play Again</Button>
+      </div>
      
-    />
     </div>
-  )
-}
+  );
+};
 
-export default Score
+export default Score;
